8b6a3d42c2600fa980fa3f20c542d43529c8a04c
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-06-30 16:54:38 +0200

    add rootfile parser

---
+++ Dumbledraw/rootfile_parser_ntuple_processor_inputshapes.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import logging
+import ROOT
+import copy
+
+logger = logging.getLogger(__name__)
+
+
+class Rootfile_parser(object):
+
+    _dataset_map = {
+        "data": "data",
+        "ZTT": "DY",
+        "ZL": "DY",
+        "ZJ": "DY",
+        "TTT": "TT",
+        "TTL": "TT",
+        "TTJ": "TT",
+        "VVT": "VV",
+        "VVL": "VV",
+        "VVJ": "VV",
+        "W": "W",
+        "EMB": "EMB",
+        "QCDEMB": "QCD",
+        "QCD": "QCDMC",
+        "jetFakesEMB": "jetFakes",
+        "jetFakes": "jetFakesMC",
+        "ggH125": "ggH",
+        "qqH125": "qqH",
+    }
+
+    _process_map = {
+        "data": "data",
+        "ZTT": "DY-ZTT",
+        "ZL": "DY-ZL",
+        "ZJ": "DY-ZJ",
+        "TTT": "TT-TTT",
+        "TTL": "TT-TTL",
+        "TTJ": "TT-TTJ",
+        "VVT": "VV-VVT",
+        "VVL": "VV-VVL",
+        "VVJ": "VV-VVJ",
+        "W": "W",
+        "EMB": "Embedded",
+        "QCDEMB": "QCD",
+        "QCD": "QCDMC",
+        "jetFakesEMB": "jetFakes",
+        "jetFakes": "jetFakesMC",
+        "ggH125": "ggH125",
+        "qqH125": "qqH125",
+    }
+
+    def __init__(self, inputrootfilename, variable):
+        self._rootfilename = inputrootfilename
+        self._rootfile = ROOT.TFile(self._rootfilename, "READ")
+        self._variable = variable
+
+    @property
+    def rootfile(self):
+        return self._rootfile
+
+    def get(self, channel, process, category=None, shape_type="Nominal"):
+        dataset = self._dataset_map[process]
+        if category is None:
+            category = "" if "data" in process else "-" + self._process_map[process]
+        else:
+            category = (
+                "-" + category
+                if "data" in process
+                else "-" + "-".join([self._process_map[process], category])
+            )
+        hist_hash = "{dataset}#{channel}{category}#{shape_type}#{variable}".format(
+            dataset=dataset,
+            channel=channel,
+            category=category,
+            shape_type=shape_type,
+            variable=self._variable,
+        )
+        logger.debug("Try to access %s in %s" % (hist_hash, self._rootfilename))
+        print("rootfile: ", self._rootfile.Get(hist_hash), " hash: ", hist_hash)
+
+        return self._rootfile.Get(hist_hash)
+
+    def list_contents(self):
+        return [key.GetTitle() for key in self._rootfile.GetListOfKeys()]
+
+    def get_bins(self, channel, category):
+        hist = self.get(channel, category)
+        nbins = hist.GetNbinsX()
+        bins = []
+        for i in range(nbins):
+            bins.append(hist.GetBinLowEdge(i + 1))
+        bins.append(hist.GetBinLowEdge(i + 1) + hist.GetBinWidth(i + 1))
+        return bins
+
+    def get_values(self, channel, category):
+        hist = self.get(channel, category)
+        nbins = hist.GetNbinsX()
+        values = []
+        for i in range(nbins):
+            values.append(hist.GetBinContent(i + 1))
+        return values
+
+    def __del__(self):
+        logger.debug("Closing rootfile %s" % (self._rootfilename))
+        self._rootfile.Close()
